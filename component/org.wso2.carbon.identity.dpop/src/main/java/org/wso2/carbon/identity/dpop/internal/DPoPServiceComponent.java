1e1f8cb00e0c0e8d8b472679a2f74b8274c48a3b
Isuru Hettiarachchi <isuru71@gmail.com>
2021-05-31 21:37:31 +0530

    Fix DPoPInterceptorHandler

--- component/org.wso2.carbon.identity.dpop/src/main/java/org/wso2/carbon/identity/dpop/internal/DPoPServiceComponent.java
+++ component/org.wso2.carbon.identity.dpop/src/main/java/org/wso2/carbon/identity/dpop/internal/DPoPServiceComponent.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License
+ */
+
 package org.wso2.carbon.identity.dpop.internal;
 
 import org.apache.commons.logging.Log;
@@ -10,7 +28,7 @@ import org.wso2.carbon.identity.dpop.listener.OauthDPoPInterceptorHandlerProxy;
 import org.wso2.carbon.identity.oauth.event.OAuthEventInterceptor;
 
 @Component(
-        name = "org.wso2.carbon.identity.oauth.dpop.internal.DPoPServiceComponent",
+        name = "org.wso2.carbon.identity.oauth.dpop.listener.oauth.dpopinterceptorhandler",
         immediate = true
 )
 public class DPoPServiceComponent {
@@ -18,14 +36,14 @@ public class DPoPServiceComponent {
 
     @Activate
     protected void activate(ComponentContext context) {
-        context.getBundleContext().registerService(OAuthEventInterceptor.class, new OauthDPoPInterceptorHandlerProxy(), null);
-        if (log.isDebugEnabled()) {
-            log.debug("DPoP Interceptor is Activated");
+        try {
+            context.getBundleContext().registerService(OAuthEventInterceptor.class, new OauthDPoPInterceptorHandlerProxy(),
+                    null);
+            if (log.isDebugEnabled()) {
+                log.debug("DPoP Interceptor is Activated");
+            }
+        } catch (Throwable e) {
+            log.debug("Error while registering DPoP Interceptor", e);
         }
-    }
-
-    @Deactivate
-    protected void deActivate(ComponentContext context) {
-
     }
 }
